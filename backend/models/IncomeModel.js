db0e4228d32bedd21e8dd0e57cb81e3f5811b22f
DakshBaxi <baxidaksh2004@gmail.com>
2024-05-17 01:49:51 +0530

    first commit

---
+++ backend/models/IncomeModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+
+const IncomeSchema = new mongoose.Schema({
+    amount: {
+        type: Number,
+        required: true,
+        maxLength: 20,
+        trim: true
+    },
+    type: {
+        type: String,
+        default:"income"
+    },
+    date: {
+        type: Date,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLength: 20,
+        trim: true
+    },
+}, {timestamps: true})
+
+module.exports = mongoose.model('Income', IncomeSchema)
